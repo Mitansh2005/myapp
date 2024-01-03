f8dac7869df5014a92655a3f1eae68519c90b44b
Mitansh Pithadia <mitanshpithadia76@gmail.com>
2024-01-03 23:38:01 +0530

    version 3

--- myapp/settings.py
+++ myapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e7w06ajcpq94d0$kt5l+0!@71#!_u!9-2u4f!9qez_4znv%jb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
