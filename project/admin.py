cd2ad876504efc93f8ac44e09fa8db254383276c
Mitansh Pithadia <mitanshpithadia76@gmail.com>
2024-01-08 12:49:53 +0530

    version 30

--- project/admin.py
+++ project/admin.py
@@ -1,9 +1,25 @@
+from datetime import datetime
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
+from rangefilter.filters import (
+    DateRangeFilterBuilder,
+    DateTimeRangeFilterBuilder,
+    NumericRangeFilterBuilder,
+    DateRangeQuickSelectListFilterBuilder,
+)
 from .models import daily_log
 # Register your models here.
 
 class daily_logAdmin(admin.ModelAdmin):
   list_display= ('Id','Script_Name','Client_Name','Client_Code','Buy_Date','Buy_Rate','Buy_Quantity','Buy_Amount','Sell_Date','Sell_Rate','Sell_Quantity','Sell_Amount','Net_Quantity','Net_Pl')
-  list_filter=('Id','Buy_Date','Sell_Date','Client_Code')
+  list_filter=('Id','Buy_Date','Sell_Date','Client_Code',("Buy_Date", DateRangeFilterBuilder()),
+                (
+            "Sell_Date",
+            DateTimeRangeFilterBuilder(
+                title="Sell Date",
+                default_start=datetime(2020, 1, 1),
+                default_end=datetime(2030, 1, 1),
+            )),
+        ("Buy_Date", DateRangeQuickSelectListFilterBuilder()),  # Range + QuickSelect Filter
+    )
 admin.site.register(daily_log,daily_logAdmin)
--- project/models.py
+++ project/models.py
@@ -9,21 +9,15 @@ class daily_log(models.Model):
   Buy_Rate=models.FloatField()
   Buy_Quantity=models.IntegerField()
   Buy_Amount=models.FloatField(default="",null=False,editable=False)
-  Sell_Date=models.DateField(default="",null=True)
-  Sell_Rate=models.FloatField(default="0",null=True)
-  Sell_Quantity=models.IntegerField(default="0",null=True)
-  Sell_Amount=models.FloatField(default="0",null=True,editable=False)
+  Sell_Date=models.DateField(default="",null=True,blank=True)
+  Sell_Rate=models.FloatField(default="0",null=True,blank=True)
+  Sell_Quantity=models.IntegerField(default="0",null=True,blank=True)
+  Sell_Amount=models.FloatField(default="0",null=True,editable=False,blank=True)
   Net_Quantity=models.IntegerField(default="",null=False,editable=False)
   Net_Pl=models.FloatField(default=0.0,null=False,editable=False)
 
   class Meta:
     ordering=['Id']
-
-  def __init__(self, *args, **kwargs):
-    super(daily_log, self).__init__(*args, **kwargs)
-    self.Sell_Rate.required=False
-    self.Sell_Quantity.required=False
-    self.Sell_Amount.required=False
   
   
   def save(self,*args, **kwargs):
